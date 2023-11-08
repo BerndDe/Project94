84f3df7189a4ccbb1b286b3d345768b7bcff2f11
BerndDe <95975614+BerndDe@users.noreply.github.com>
2023-11-08 16:14:48 +0000

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBX622DUzT_6M2ISuhC3VEPsU7Gr8UiEDQ",
+    authDomain: "letschat-6a9fc.firebaseapp.com",
+    databaseURL: "https://letschat-6a9fc-default-rtdb.firebaseio.com",
+    projectId: "letschat-6a9fc",
+    storageBucket: "letschat-6a9fc.appspot.com",
+    messagingSenderId: "291545520089",
+    appId: "1:291545520089:web:7be04a040ea0c65e4aebc8"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
